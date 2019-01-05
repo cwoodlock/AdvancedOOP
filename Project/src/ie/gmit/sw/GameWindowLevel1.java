16350c683b1d934c4dae637686fe82736399e52a
cwoodlock <g00341460@gmit.ie>
2019-01-05 20:33:30 +0000

    Added class for returning model for level 1

---
+++ Project/src/ie/gmit/sw/GameWindowLevel1.java
@@ -0,0 +1,46 @@
+package ie.gmit.sw;
+
+public class GameWindowLevel1 extends GameWindowModel{
+	
+	//Default return
+	private static GameWindowLevel1 instance;
+	
+	//Constructor to create level 1
+	public GameWindowLevel1() {
+		int[][] model = { 
+				{ 1, 0, 0, 0, 0, 0 , 0, 0, 0, 2},
+				{ 0, 1, 0, 0, 0, 0 , 0, 0, 0, 2},
+				{ 0, 0, 2, 0, 0, 0 , 0, 0, 0, 2},
+				{ 0, 0, 0, 1, 0, 0 , 0, 0, 0, 2},
+				{ 2, 2, 2, 2, 1, 0 , 0, 0, 0, 2},
+				{ 3, 3, 3, 3, 1, 1 , 1, 0, 0, 1},
+				{ 5, 5, 5, 5, 3, 3 , 1, 0, 0, 1},
+				{ 4, 4, 4, 5, 3, 3 , 1, 0, 0, 0},
+				{ 4, 4, 4, 4, 5, 3 , 1, 6, 6, 6},
+				{ 4, 4, 4, 4, 4, 3 , 1, 7, 7, 7}
+		};
+		
+		int[][] objects = { 
+				{ 0, 0, 0, 5, 5, 5 , 5, 5, 5, 0},
+				{ 5, 0, 0, 0, 5, 5 , 5, 5, 5, 0},
+				{ 5, 5, 0, 0, 0, 5 , 5, 5, 5, 9},
+				{ 5, 5, 2, 0, 0, 0 , 5, 5, 5, 0},
+				{ 0, 0, 0, 0, 0, 0 , 0, 5, 5, 0},
+				{ 0, 0, 0, 0, 0, 0 , 0, 0, 5, 0},
+				{ 0, 0, 0, 0, 0, 3 , 0, 0, 0, 0},
+				{ 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0},
+				{ 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0},
+				{ 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0}
+		};
+	}
+	
+	//if its null set return to the aboove level else instance must not be null so return that instead
+	public static GameWindowModel getInstance() {
+		if(instance == null) {
+			return new GameWindowLevel1();
+		}else {
+			return instance;
+		}
+	}
+
+}
